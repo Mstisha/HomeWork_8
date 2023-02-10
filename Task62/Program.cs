91765c349d0b718bf5fee4259a71671d52b7a9de
Slava <mistik99@mail.ru>
2023-02-10 16:08:49 +0200

    Задача 62

---
+++ Task62/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j].ToString("00")} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[4, 4];
+
+for (int i = 1; i < 5; i++)
+    matrix[0, i - 1] = i;
+for (int i = 5; i < 8; i++)
+    matrix[i - 4, 3] = i;
+for (int i = 8; i < 11; i++)
+    matrix[3, 10 - i] = i;
+for (int i = 11; i < 13; i++)
+    matrix[13 - i, 0] = i;
+for (int i = 13; i < 15; i++)
+    matrix[1, i - 12] = i;
+for (int i = 15; i < 17; i++)
+    matrix[2, 17 - i] = i;
+
+PrintMatrix(matrix);
